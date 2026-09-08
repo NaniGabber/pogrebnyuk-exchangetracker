58aec11c08ca0329254635b606a0b95983dde389
Alexandr Pogrebnyuk <nani.lennauchpage@gmail.com>
2026-09-08 18:34:57 +0000

    added simple code to work with privatbankAPI

--- src/exchangetracker/cli.py
+++ src/exchangetracker/cli.py
@@ -1,5 +1,17 @@
+import requests
+
 def main() -> None:
-    print("ExchangeTracker 0.0.1 — середовище налаштовано")
+   
+    url = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"
+
+    response = requests.get(url)
+    data = response.json()
+
+    print("Курси валют ПриватБанк:")
+    for item in data:
+        print(f"{item['ccy']} -> {item['base_ccy']}: "
+              f"Buy = {item['buy']}, Sell = {item['sale']}")
+
 
 
 if __name__ == "__main__":
